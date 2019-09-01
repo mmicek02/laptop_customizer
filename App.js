32968095d1aadf029e48d78449d42ac378d4b273
Michael Micek <49984452+mmicek02@users.noreply.github.com>
2019-09-01 16:06:55 -0500

    updated and new files made for assignment

---
+++ App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './App.css';
+import ProductTable from './ProductTable/ProductTable';
+import CustomLaptopOptions from './CustomLaptopOptions/CustomLaptopOptions';
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class App extends Component {
+  state = {
+    selected: {
+      Processor: {
+        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        cost: 700
+      },
+      'Operating System': {
+        name: 'Ubuntu Linux 16.04',
+        cost: 200
+      },
+      'Video Card': {
+        name: 'Toyota Corolla 1.5v',
+        cost: 1150.98
+      },
+      Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+    }
+  };
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+    //
+    const total = Object.keys(this.state.selected).reduce(
+      (acc, curr) => acc + this.state.selected[curr].cost,
+      0
+    );
+
+    return (
+      // Displays the total of the all of the selected features of the laptop
+      <div className="App">
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+        <main>
+          <form className="main__form">
+            <h2>Customize your laptop</h2>
+            <CustomLaptopOptions 
+              features = {this.props.features} />
+          </form>
+          <section className="main__summary">
+            <h2>Your cart</h2>
+            <ProductTable 
+                summary = {this.state.summary} />
+            <div className="summary__total">
+              <div className="summary__total__label">Total</div>
+              <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    );
+  }
+  
+}
+export default App;
---
+++ CustomLaptopOptions/CustomLaptopOptions.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import '../App.css';
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
+
+class CustomLaptopOptions extends Component {
+    render() {
+        console.log(this.props);
+        // This takes the features from the array FEATURES and allows them to be manipulated
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+            const itemHash = slugify(JSON.stringify(item));
+            return (
+                <div key={itemHash} className="feature__item">
+                    <input
+                        type="radio"
+                        id={itemHash}
+                        className="feature__option"
+                        name={slugify(feature)}
+                        checked={item.name === this.state.selected[feature].name}
+                        onChange={e => this.updateFeature(feature, item)}
+                    />
+                    <label htmlFor={itemHash} className="feature__label">
+                        {item.name} ({this.USCurrencyFormat.format(item.cost)})
+                    </label>
+                </div>
+                );
+            });
+
+            return (
+                // This displays the features in a way the user can interact with 
+                <fieldset className="feature" key={featureHash}>
+                    <legend className="feature__name">
+                        <h3>{feature}</h3>
+                    </legend>
+                    {options}
+                </fieldset>
+            );
+        });
+    }
+}
+export default CustomLaptopOptions;
